04e2eec35abc0622555f303a181d0ca50e29c23d
Buterat4life <106036034+Buterat4life@users.noreply.github.com>
2024-05-09 20:05:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var helmet = require('helmet');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,16 +8,40 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var searchRouter = require('./routes/search');
 var apiRouter = require('./routes/api');
-const robots = require('express-robots-txt')
+var robots = require('express-robots-txt')
 
 var app = express();
+
+
+
+app.disable('x-powered-by');
+
+
+
+// Установка заголовка Content-Security-Policy
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"], // Разрешаем загрузку ресурсов только из текущего источника
+      scriptSrc: ["'self'"], // Разрешаем загрузку скриптов только из текущего источника
+      styleSrc: ["'self'"], // Разрешаем загрузку стилей только из текущего источника
+      fontSrc: ["'self'"], // Разрешаем загрузку шрифтов только из текущего источника
+      imgSrc: ["'self'"], // Разрешаем загрузку изображений только из текущего источника
+    },
+  })
+);
+
+
+
+// Добавление заголовка X-Content-Type-Options
+app.use(helmet.xContentTypeOptions());
+
 
 app.use(robots({
     UserAgent: '*',
     Disallow: [ '/', '/search' ]
 }));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -30,18 +55,16 @@ app.use('/', indexRouter);
 app.use('/search', searchRouter);
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
