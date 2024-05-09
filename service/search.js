37ba2c8a91ad9b713f256c96e993315d40099d32
Buterat4life <106036034+Buterat4life@users.noreply.github.com>
2024-05-09 20:07:22 +0300

    Update search.js

--- service/search.js
+++ service/search.js
@@ -2,15 +2,11 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/vulny.db');
 
 const searchByName = function (searchText, cb) {
-
-  db.all("select id,name,description from item where name like '%" + searchText + "%'", [], function (err, rows) {
-  //db.all("select id,name,description from item where name like '%?%'", [searchText], function (err, rows) {
+  db.all("SELECT id, name, description FROM item WHERE name LIKE ?", ['%' + searchText + '%'], function (err, rows) {
     if (err) {
       console.log('error ' + err);
       cb(err);
-      //res.redirect('/search')
     } else {
-      //console.log(req.body);
       console.log('rows? ' + rows);
       console.log('got ' + rows.length + ' rows');
       const ret = {
@@ -18,10 +14,8 @@ const searchByName = function (searchText, cb) {
         rows: rows
       };
       cb(null, ret);
-      //res.render('searchResult', ret)
     }
   });
-
 };
 
 module.exports = {
